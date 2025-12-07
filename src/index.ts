3316575cb28a56ccacf1eaa63e369f7ce28af049
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2025-12-07 16:28:43 +0530

    added cors and csrf protection on the server side

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,59 @@
 import 'dotenv/config';
 import express from 'express';
+import CookieParser from 'cookie-parser';
 import bookImageRouter from './routes/book_image.router';
 // FIX: Use a dot-slash for files in the same or sub-directory.
 import authorRouter from './routes/author.router';
 import bookRouter from './routes/book.router';
+import cors from 'cors';
+import csrf from 'csurf';
 const app=express()
+// app.options('/delete/:id',cors());// preflight 
+
+// app.use(cors())
 const port=process.env.PORT || 8080;
 
+const TRUSTED_ORIGINS=(process.env.TRUSTED_ORIGINS || "").split(',').map(s=>s.trim()).filter(Boolean);
+function isTrusted(origin?:string){
+    return origin && TRUSTED_ORIGINS.includes(origin);
+}
+
+const protectedMethod=['POST','GET','DELETE'];
+
+const protectedPaths=['/auth','/book/'];
+
+const dynamicCors=(req,callback)=>{
+    const origin=req.header('origin');
+    const method=req.method.toUpperCase();
+    const path=req.path;
+    const pathIsSensitive=protectedPaths.some(p=>path.startsWith(p));
+    const methodIsSensitive=protectedMethod.some(method);
+    if(pathIsSensitive || methodIsSensitive){
+        if(isTrusted(origin)){
+            return callback(null,{
+                origin:origin,
+                credentials:true,
+                methods:['GET','POST','DELETE','HEAD','PATCH','PUT'],
+                allowedHeaders:['Content-Type','Authorization'],
+            })
+        }
+        else{
+            return callback(null,{origin:false});
+        }
+    }
+    return callback(null,{origin:'*'});
+}
+app.use(cors(dynamicCors));
 app.use(express.json())
+app.use(CookieParser());
+const csrfProtection=csrf({
+    cookie:{
+        httpOnly:true,
+        secure:true,
+        sameSite:"strict",
+    },
+})
+app.use(csrfProtection);
 app.use('/bookImage',bookImageRouter);
 app.use('/authors',authorRouter);
 app.use('/books',bookRouter);
--- src/routes/book_image.router.ts
+++ src/routes/book_image.router.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
 import { postImage,multipleUploadFiles, getImage, deleteImage } from '../controllers/book_image.controller';
-
+import cors from 'cors';
 const bookImage=Router();
 
 bookImage.post('/',multipleUploadFiles,postImage);
 bookImage.get('/',multipleUploadFiles,getImage);
-bookImage.delete('/delete/:id',deleteImage);
+bookImage.delete('/delete/:id',cors(),deleteImage);
+bookImage.options('/delete/:id',cors());
 export default bookImage;
